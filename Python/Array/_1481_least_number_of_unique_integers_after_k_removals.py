2e6d62edcd3e3c2363413129916eefde3d4da38b
Sivaprakash161999 <sivaprakash161999@gmail.com>
2024-02-17 10:50:25 +0530

    added 1481 solution

---
+++ Python/Array/_1481_least_number_of_unique_integers_after_k_removals.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        '''Count the frequency of each number.
+           Sort the frequencies;
+           Start removing elements with lowest frequencies upto K elements.
+           Return the number of remaining elements'''
+        c = Counter(arr)
+        ans = len(c)
+        for f in sorted(c.values()):
+            if f <= k:
+                k -= f
+                ans -= 1
+            else:
+                return ans
+        return ans
+
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        # using a heap, instead of sorting
+        # to pick elements with least frequency
+        c = Counter(arr)
+        ans = len(c)
+        freq = list(c.values())
+        heapq.heapify(freq)
+        while freq:
+            ele = heapq.heappop(freq)
+            if ele <= k:
+                k -= ele
+                ans -= 1
+            else:
+                break
+        return ans
