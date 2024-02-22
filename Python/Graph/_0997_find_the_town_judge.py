5af9dcaf269d4caa812c9498085bc1b479c963b1
Sivaprakash161999 <sivaprakash161999@gmail.com>
2024-02-22 11:07:44 +0530

    added 997

--- Python/Graph/_0997_find_the_town_judge.py
+++ Python/Graph/_0997_find_the_town_judge.py
@@ -2,8 +2,11 @@ from typing import List
 
 
 class Solution:
-    # Graph - indegree vs outdegree time - O(n) space - O(n)
+    # Graph - indegree vs outdegree; time - O(n) space - O(n)
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        '''The judge will have an indegree of n-1
+            and out-degree of 0;
+            so cummulative in-out degree is n-1'''
         in_out_bound = [0] * (n + 1)
         for f, t in trust:
             in_out_bound[f] -= 1
